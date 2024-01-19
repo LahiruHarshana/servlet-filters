6a91d475b13b255fc4a77331fa322a5ea30583bd
LahiruHarshana <lharshana2002@gmail.com>
2024-01-19 13:01:26 +0530

    first commit

---
+++ src/main/java/ijse/lk/servletfilter/api/servletfilters/api/ItemServlet.java
@@ -0,0 +1,8 @@
+package ijse.lk.servletfilter.api.servletfilters.api;
+
+
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet(name = "itemServlet", value = "/item-servlet")
+public class ItemServlet {
+}
