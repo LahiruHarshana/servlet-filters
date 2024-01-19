264b1f403cd3b5212678d65746158df4d42e35d3
LahiruHarshana <lharshana2002@gmail.com>
2024-01-19 13:01:54 +0530

    first commit

--- src/main/java/ijse/lk/servletfilter/api/servletfilters/api/ItemServlet.java
+++ src/main/java/ijse/lk/servletfilter/api/servletfilters/api/ItemServlet.java
@@ -2,7 +2,9 @@ package ijse.lk.servletfilter.api.servletfilters.api;
 
 
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 
 @WebServlet(name = "itemServlet", value = "/item-servlet")
-public class ItemServlet {
+public class ItemServlet extends HttpServlet {
+
 }
