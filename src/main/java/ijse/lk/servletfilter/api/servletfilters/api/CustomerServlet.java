4631adbaeb535dddc81aadc3ef4f879de9af0395
LahiruHarshana <lharshana2002@gmail.com>
2024-01-19 12:53:31 +0530

    first commit

--- src/main/java/ijse/lk/servletfilter/api/servletfilters/api/CustomerServlet.java
+++ src/main/java/ijse/lk/servletfilter/api/servletfilters/api/CustomerServlet.java
@@ -1,10 +1,19 @@
 package ijse.lk.servletfilter.api.servletfilters.api;
 
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet(name = "customerServlet", value = "/customer-servlet")
 public class CustomerServlet extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Customer Servlet: incoming request");
+    }
 }
