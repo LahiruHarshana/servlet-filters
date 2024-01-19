0c2cd3a07c8b278d61132e46d9b7044aca9d64cb
LahiruHarshana <lharshana2002@gmail.com>
2024-01-19 13:02:08 +0530

    first commit

--- src/main/java/ijse/lk/servletfilter/api/servletfilters/api/ItemServlet.java
+++ src/main/java/ijse/lk/servletfilter/api/servletfilters/api/ItemServlet.java
@@ -1,10 +1,18 @@
 package ijse.lk.servletfilter.api.servletfilters.api;
 
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet(name = "itemServlet", value = "/item-servlet")
 public class ItemServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+    }
 }
