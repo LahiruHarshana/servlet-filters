71c577a51ee278f36896696fdc0ac57e56d8c8fa
LahiruHarshana <lharshana2002@gmail.com>
2024-01-19 12:35:28 +0530

    first commit

--- src/main/java/ijse/lk/servletfilter/api/servletfilter/filter/CORSFilter.java
+++ src/main/java/ijse/lk/servletfilter/api/servletfilter/filter/CORSFilter.java
@@ -1,4 +1,8 @@
 package ijse.lk.servletfilter.api.servletfilter.filter;
 
+
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet(name = "corsFilter", value = "/cors-filter")
 public class CORSFilter {
 }
