24579054bbeae7d08765fd92e89cd3933d75acd3
LahiruHarshana <lharshana2002@gmail.com>
2024-01-19 12:36:41 +0530

    first commit

--- src/main/java/ijse/lk/servletfilter/api/servletfilter/filter/CORSFilter.java
+++ src/main/java/ijse/lk/servletfilter/api/servletfilter/filter/CORSFilter.java
@@ -1,8 +1,19 @@
 package ijse.lk.servletfilter.api.servletfilter.filter;
 
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet(name = "corsFilter", value = "/cors-filter")
-public class CORSFilter {
+public class CORSFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        System.out.println("CORS Filter");
+    }
 }
